a82df32bdcd379d0f314e939d161f09f1edd4562
Janne Hellsten <jjhellst@gmail.com>
2012-01-04 20:50:34 +0200

    Add some examples of using parameters

--- Test2.hs
+++ Test2.hs
@@ -15,7 +15,7 @@ reltrack list errors
 
 globalOpts = []
 
-cmds = 
+cmds =
   [
     Cmd "new-run" [ReqArg "job"],
     Cmd "set-error" [ReqArg "run-id"]
@@ -25,6 +25,10 @@ report :: Either String a -> IO ()
 report (Right s) = return ()
 report (Left err) = putStrLn err >> exitFailure
 
+reportOrRun :: (a -> IO ()) -> Either String a -> IO ()
+reportOrRun f (Right args) = f args
+reportOrRun _ (Left err)  = putStrLn err >> exitFailure
+
 requireOptStrArg :: OptMap -> String -> Either String String
 requireOptStrArg opts s =
   case M.lookup s opts of
@@ -32,19 +36,26 @@ requireOptStrArg opts s =
       maybe (Left $ "no value for option '" ++ s ++ "'") return v
     Nothing ->
       Left ("option '" ++ s ++ "' is required but not given")
-  
+
 handleNewRun :: OptMap -> [String] -> IO ()
 handleNewRun lopts files =
-  report $ do
+  reportOrRun newRun $ do
     jobId <- requireOptStrArg lopts "job"
-    return ()
+    return jobId
+  where
+    newRun jobId = putStrLn ("new-run "++show jobId)
 
+handleSetError :: OptMap -> [String] -> IO ()
 handleSetError lopts files =
-  return ()
-  
+  reportOrRun newRun $ do
+    runId <- requireOptStrArg lopts "run-id"
+    return runId
+  where
+    newRun runId = putStrLn ("set-error for run-id="++show runId)
+
 handle :: Either String (OptMap, String, OptMap, [String]) -> IO ()
 handle (Right (gopts,cmd,lopts,files)) =
-  case cmd of 
+  case cmd of
     "new-run" -> handleNewRun lopts files
     "set-error" -> handleSetError lopts files
 handle l@(Left _) = report l
